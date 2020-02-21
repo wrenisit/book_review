ada4aee4075bf49513af5cd2be3600e7d46fdedc
Wren Steitle <lauren.steitle@gmail.com>
2020-02-21 11:13:20 -0700

    Add subjects to show

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
     book = book_conn.get("/search.json?title=#{params[:title]}")
     book_response = JSON.parse(book.body)
     @book = book_response["docs"][0]
+    
     review_response = Faraday.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{params[:title]}&api-key=zwhbSB78zpZbugeLb8MsAUCUHB2JKQq6")
     reviews = JSON.parse(review_response.body)
     @reviews = reviews["results"]
